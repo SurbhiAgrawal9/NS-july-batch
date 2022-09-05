6c1cd0db4874788cd28bd7e2a599ea7c96c2e210
SurbhiAgrawal9 <111246726+SurbhiAgrawal9@users.noreply.github.com>
2022-09-05 10:56:20 +0530

    Update Area of Square.java

--- Basic Programinh in Java/Area of Square.java
+++ Basic Programinh in Java/Area of Square.java
@@ -1,8 +1,7 @@
 import java.io.*; 
 import java.util.*;
 
-// don't change the name of this class
-// you can add inner classes if needed
+
 class Main {
 
     public static void main (String[] sun) {
